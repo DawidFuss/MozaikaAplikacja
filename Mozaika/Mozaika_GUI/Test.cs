3ab117f6338a22b0f49fffdf40d59c79b7ce5cab
Piotr-lesniak-141081 <ageofprogramming@gmail.com>
2023-09-14 10:57:12 +0200

    Zrobienie przycisku i dodanie klasy 123

---
+++ Mozaika/Mozaika_GUI/Test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mozaika_GUI
+{
+    class Test
+    {
+        public void PzestawSie()
+        {
+            Console.WriteLine("to ja klasa testowa");
+        }
+    }
+}
