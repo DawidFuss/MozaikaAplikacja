5918a901f72cfad2530c0f0dd58659b723ea1be6
DawidFuss <143649850+DawidFuss@users.noreply.github.com>
2023-09-27 18:16:37 +0200

    dodanie skryptu

---
+++ Mozaika/Mozaika_GUI/Skrypt.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mozaika_GUI
+{
+    internal class Skrypt
+    {
+
+        public void ChangeExtension(string extension)
+        {
+            DirectoryInfo f = new DirectoryInfo(extension);
+            if (f.Exists)
+            {
+
+                string targetFolder = "";
+                var jpgFiles = f.GetFiles("*jpg.", SearchOption.AllDirectories);
+                foreach (var files in jpgFiles)
+                {
+
+                    if (Path.GetExtension(files.Name) != ".jpg")
+                    {
+
+                        string newFileName = Path.ChangeExtension(files.FullName, ".jpg");
+                        files.MoveTo(targetFolder);
+
+                    }
+
+                }
+
+
+
+            }
+            else
+            {
+                return;
+            }
+
+
+        }
+
+    }
+}
