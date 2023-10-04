a5e63e16819a5541b640b72657c7bfd9ba91b4f3
DawidFuss <143649850+DawidFuss@users.noreply.github.com>
2023-10-04 20:35:40 +0200

    Dodanie GUI

--- Mozaika/Mozaika_GUI/Form1.cs
+++ Mozaika/Mozaika_GUI/Form1.cs
@@ -10,9 +10,23 @@ namespace Mozaika_GUI
 {
     public partial class Form1 : Form
     {
+    
         public Form1()
         {
+           
             InitializeComponent();
+            ListView1.View = View.Details;
+            ListView1.Columns.Add("Nazwa zbioru", 100);
+            ListView1.Columns.Add("Ilość Elementów", 100);
+            ListView1.Columns.Add("Opis", 100);
+            ListView1.Columns.Add("Ścieżka", 150);
+            
+            string[] elementy_Rozmiar_Mozaiki = { "A0: 841mm x 1189mm", "A1: 594mm x 841mm", "A2: 420mm x 594mm",
+                "A3: 297mm x 420mm","A4: 210mm x 297mm"};
+            comboBox1.Items.AddRange(elementy_Rozmiar_Mozaiki);
+
+
+
         }
 
         private void przycisk_Click(object sender, EventArgs e)
@@ -21,34 +35,32 @@ namespace Mozaika_GUI
             //this.przycisk.Text = textBox1.Text;
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            // Skrypt.ChangeExtension(@"C:\Users\dawid\OneDrive\Desktop\zdjecia");
-            ListaMiniaturek miniaturki = new ListaMiniaturek();
-            miniaturki.Wczytaj(@"C:\Users\dawid\OneDrive\Desktop\zdjecia");
-            Obraz obraz = new Obraz(@"C:\Users\dawid\OneDrive\Desktop\Mozaiki\images(8).jpg", miniaturki);
-            obraz.ZrobMozaike();
-            Class1 x = new Class1();
 
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
+        
 
         private void button1_Click_1(object sender, EventArgs e)
         {
 
             OpenFileDialog openFileDialog = new OpenFileDialog();
-            openFileDialog.Filter = "Text files (*.txt) | *.txt | All.files (*.*)|*.*";
+            openFileDialog.Filter = "Text files (*.jpg) | *.jpg | All.files (*.*)|*.*";
             openFileDialog.Title = "Wybierz plik";
             if (openFileDialog.ShowDialog() == DialogResult.OK)
             {
-                string selectedPath = openFileDialog.FileName;
+                textBoxImageName.Text = openFileDialog.FileName;
             }
 
         }
+
+       
+
+        private void button1_Click_2(object sender, EventArgs e)
+        {
+            ListaMiniaturek miniaturki = new ListaMiniaturek();
+            miniaturki.Wczytaj(@"C:\Users\dawid\OneDrive\Desktop\zdjecia");
+            Obraz obraz = new Obraz(@"C:\Users\dawid\OneDrive\Desktop\Mozaiki\images(8).jpg", miniaturki);
+            obraz.ZrobMozaike();
+        }
+
+       
     }
 }
